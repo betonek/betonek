fb01ac0a8fc3e514c2ea959d5841f212ca9ef16d
Marek Wawrzyczek <mwawrzyczek@gmail.com>
2011-05-14 12:04:31 +0200

    removed old code and improvements to autocompletion

--- htdocs/js/add.js
+++ htdocs/js/add.js
@@ -1,50 +1,20 @@
-var config_search_box = function(elem, url, printer){
-    printer = printer || function(obj){ return obj; };
-    var $cont = $(elem);
-    var $res = $cont.find('[name=result]').eq(0),
-        $txt_query = $cont.find('[name=query]').eq(0),
-        $err = $cont.find('[name=error]').eq(0);
-    var default_val = "None objects returned";
-    var indicate_no_result = function(){ $res.children().remove(); $res.append($("<span>").html(default_val)); },
-        clear_error = function(){ $err.html(''); };
-
-    indicate_no_result();
-
-    var on_change = function(){
-        var val = $.trim( $txt_query.val() );
-        if(!val.length){
-            $err.html('trimmed search criteria have length==0');
-            indicate_no_result();
-        } else {
-            clear_error();
-            $.rpc(url, { 'query': $txt_query.val() },
-               function(data){
-                   $res.children().remove();
-                   console.log(data);
-                   objs = data.objects;
-                   if(objs.length){
-                        for(var i=0;i<objs.length;++i)
-                           $res.append( printer(objs[i]) );
-                   } else {
-                       indicate_no_result();
-                   }
-               });
-        }
-    };
-
-    $txt_query.keyup(on_change);
-}
-
 $(document).ready(function(){
     $("#search_box [name=query]").autocomplete({
         source: function(request, response){
             $.rpc("search_authors", { 'query': request.term },
                function(data){
                     objs = $.map(data.objects, function(obj){ return $.extend(obj, {'label': obj.author_name });} );
+                    if(!objs.length)
+                        objs = [{'label': "No elements matching criteria: " + request.term, "REPRESENTS_NULL": true}];
                     response(objs);
             });
         },
         select: function(event, ui){
+            if(objs.length == 1 && objs[0].REPRESENTS_NULL){
+                alert("no authors found found");
+                event.preventDefault();
+                return;
+            }
             $("#add [name=author_id]").val(ui.item.author_id);
             $("#add [name=author]").val(ui.item.author_name);
         },

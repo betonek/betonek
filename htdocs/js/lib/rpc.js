ac2f970ca81f8e1e1712b1b210fcc3364b1eddb7
Paweł Foremski <pawel@foremski.pl>
2012-02-23 20:18:46 +0100

    rpc: show busy cursor, handle errors

--- htdocs/js/lib/rpc.js
+++ htdocs/js/lib/rpc.js
@@ -7,17 +7,49 @@
  * @param options {Object}  additional JQuery AJAX options (see docs) */
 $.rpc = $.rpc || function(method, params, cb, errcb, options)
 {
+	var $body = $("body");
+
+	$body.addClass("busy");
+	$body.data("rpc_waiting", 1 + ($body.data("rpc_waiting") || 0));
+
+	var handle_return = function()
+	{
+		var waiting = $body.data("rpc_waiting") - 1;
+
+		if (waiting <= 0) {
+			$body.data("rpc_waiting", 0);
+			$("body").removeClass("busy");
+		} else {
+			$body.data("rpc_waiting", waiting);
+		}
+	};
+
 	return $.ajax($.extend({
 		contentType: 'application/json',
 		type: 'POST',
 		url: "rpc.php",
 		cache: false,
 		success: function(data) {
+			handle_return();
 			if (cb && data.result)
 				cb(data.result, method);
 			else if (errcb && data.error)
 				errcb(data.error, method);
 		},
+		error: function(jqXHR, status, exception)
+		{
+			handle_return();
+
+			if (status == "error") {
+				alert("RPC error: " + exception);
+			} else if (status == "timeout") {
+				/* repeat the query */
+				$.rpc(method, params, cb, errcb, options);
+			} else {
+				alert("RPC " + status + " (" + exception + ")");
+			}
+		},
+		timeout: 10000,
 		data: JSON.stringify({ method: method, params: params })
 	}, options));
 }})(jQuery);
